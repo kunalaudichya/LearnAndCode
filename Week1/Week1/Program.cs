17e2accde42d4083d27d046cbaf541b47da1fbad
Kunal Audichya <kunal.audichya@intimetec.com>
2025-12-16 23:26:37 +0530

    refactor: print adjacent country names instead of just country name

--- Week1/Week1/Program.cs
+++ Week1/Week1/Program.cs
@@ -8,23 +8,22 @@ namespace Week1
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
             var countryService = new CountryService();
 
-            Console.WriteLine("GUESS THE COUNTRY");
+            Console.WriteLine("GUESS THE ADJACENT COUNTRIES");
 
             while (true)
             {
-                Console.WriteLine("Enter any Country Code or type 0 to exit: ");
+                Console.WriteLine("Enter any Country Code(IN/US/NZ) or type 0 to exit: ");
                 string code = Console.ReadLine()?.Trim() ?? "";
 
                 if (code.Equals("0", StringComparison.OrdinalIgnoreCase))
                 {
                     break;
                 }
-                string result = countryService.GetCountryName(code);
+                string result = countryService.GetAdjacentCountryName(code);
                 Console.WriteLine($"Result: {result}\n");
             }
         }
