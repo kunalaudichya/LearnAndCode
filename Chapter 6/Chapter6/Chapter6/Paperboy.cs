fa9ed3b98353adab5a9bce5ff5e48cb76a6a26e1
Kunal Audichya <kunal.audichya@intimetec.com>
2026-02-10 19:24:27 +0530

    Added assignment for Chapter 6: Objects and Data Structures

---
+++ Chapter 6/Chapter6/Chapter6/Paperboy.cs
@@ -0,0 +1,15 @@
+﻿namespace Chapter6
+{
+    public class Paperboy
+    {
+        public void CollectPayment(Customer customer, float paymentAmount)
+        {
+            bool paymentCollected = customer.PayAmount(paymentAmount);
+
+            if (!paymentCollected)
+            {
+                Console.WriteLine("Come back later.");
+            }
+        }
+    }
+}
---
+++ Chapter 6/Chapter6/Chapter6/Wallet.cs
@@ -0,0 +1,26 @@
+﻿namespace Chapter6
+{
+    public class Wallet
+    {
+        private float value;
+
+        public float GetTotalMoney()
+        {
+            return value;
+        }
+
+        public void SetTotalMoney(float newValue)
+        {
+            value = newValue;
+        }
+
+        public void SubtractMoney(float debit)
+        {
+            if(value >= debit)
+            {
+                value -= debit;
+            }
+        }
+
+    }
+}
